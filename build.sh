c413d12a08e5b33ba4b7c61e0a1982c898677c89
breisa <breisa@posteo.me>
2026-04-10 14:03:54 +0200

    move localization to speparate file

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 OUT="betterlist.xdc"
-FILES=("index.html" "icon.png" "manifest.toml")
+FILES=("index.html" "langs.js" "icon.png" "manifest.toml")
 
 # Ensure zip is available
 command -v zip >/dev/null 2>&1 || { echo "zip not found" >&2; exit 2; }
