dbf5bad575bf5fdc9f1c5e9145fa5e4518b0802f
breisa <breisa@posteo.me>
2026-04-12 17:33:19 +0200

    fix build script to not leave dirty directory behind

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 OUT="betterlist.xdc"
-VERSION_FILE="./version.js"
-FILES=("./src/index.html" "./src/langs.js" "./res/icon.png" "manifest.toml" $VERSION_FILE)
+VERSION_FILE="version.js"
+FILES=("./src/index.html" "./src/langs.js" "./res/icon.png" "manifest.toml" "$VERSION_FILE")
 
 # Make sure zip is available
 command -v zip >/dev/null 2>&1 || { echo "zip not found" >&2; exit 2; }
@@ -19,3 +19,4 @@ trap 'rm -f "$VERSION_FILE"' ERR
 printf '// generated during build\nconst APP_VERSION = %s;\n' "${APP_VERSION@Q}" > "$VERSION_FILE"
 
 zip -9 -j -q "$OUT" "${FILES[@]}"
+rm -f "$VERSION_FILE"
